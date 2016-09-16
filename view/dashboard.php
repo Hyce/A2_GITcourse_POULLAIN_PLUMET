afdf15374e8edea22670f5e31caa5e3dd28a6240
Hyce <jon.poullain@gmail.com>
2016-09-16 15:49:10 +0200

    BDD

--- view/dashboard.php
+++ view/dashboard.php
@@ -26,6 +26,33 @@
 										?>
 									</div>
 								</div>
+
+								Code PHP
+
+								<?php
+								// on se connecte à MySQL
+								$db = mysql_connect('localhost', 'login', 'password');
+
+								// on sélectionne la base
+								mysql_select_db('nom_de_la_base',$db);
+
+								// on crée la requête SQL
+								$sql = 'SELECT nom,prenom,statut,date FROM famille_tbl';
+
+								// on envoie la requête
+								$req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
+
+								// on fait une boucle qui va faire un tour pour chaque enregistrement
+								while($data = mysql_fetch_assoc($req))
+								{
+									// on affiche les informations de l'enregistrement en cours
+									echo '<b>'.$data['nom'].' '.$data['prenom'].'</b> ('.$data['statut'].')';
+									echo ' <i>date de naissance : '.$data['date'].'</i><br>';
+								}
+
+								// on ferme la connexion à mysql
+								mysql_close();
+								?>
 								<div class="col-xs-10 col-sm-10 col-md-11 col-lg-11">
 									<div class="pull-right">
 										<ul class="list-inline actionicon">
