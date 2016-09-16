807d1f3248ad7e143464a6a20aa17f620702721b
Hyce <jon.poullain@gmail.com>
2016-09-16 16:57:58 +0200

    Message

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -20,7 +20,7 @@ $message = $_POST['message'];
 
 $to = 'jonathan.poullain@devinci.fr';
 $email_subject = "Website Contact Form:  $name";
-$email_body = "Vous avez reçu un nouveau message de votre site Ponts de Paris.\n\n"."Ici sont les détails\n\nNom: $name\n\nEmail: $email_address\n\nTéléphone: $phone\n\nMessage:\n$message";
+$email_body = "Vous avez reçu un nouveau message de votre site Spotify.\n\n"."Ici sont les détails\n\nNom: $name\n\nEmail: $email_address\n\nTéléphone: $phone\n\nMessage:\n$message";
 $headers = "From: jonathan.poullain@devinci.fr\n";
 $headers .= "Repondre à: $email_address";
 mail($to,$email_subject,$email_body,$headers);
