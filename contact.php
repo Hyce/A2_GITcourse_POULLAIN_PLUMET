a8fdae9057917ac0db8285039d85db07408566b8
Hyce <jon.poullain@gmail.com>
2016-09-16 16:53:41 +0200

    contact page

---
+++ contact.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+require('config/config.php');
+require('model/functions.fn.php');
+
+
+
+
+include 'view/_header.php';
+include 'view/album.php';
+include 'view/_footer.php';
